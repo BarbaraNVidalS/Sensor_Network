f85ab84a4e695fb6e9853b8efb18d413136f1fc9
BarbaraNVidalS <112970390+BarbaraNVidalS@users.noreply.github.com>
2023-03-01 19:05:23 -0600

    Add files via upload

---
+++ Sensor_test/lcd_ultrasonico.ino
@@ -0,0 +1,81 @@
+//Reference: https://github.com/sparkfun/HC-SR04_UltrasonicSensor.git
+#include  <Wire.h>
+#include  <LiquidCrystal_I2C.h>
+// Pins
+const int TRIG_PIN = 7;
+const int ECHO_PIN = 8;
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// Anything over 400 cm (23200 us pulse) is "out of range"
+const unsigned int MAX_DIST = 23200;
+
+void setup() {
+  
+  // The Trigger pin will tell the sensor to range find
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  //Set Echo pin as input to measure the duration of 
+  //pulses coming back from the distance sensor
+  pinMode(ECHO_PIN, INPUT);
+
+  // We'll use the serial monitor to view the sensor output
+  Serial.begin(9600);
+
+  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
+  lcd.begin(16, 2);
+  // Mover el cursor a la primera posición de la pantalla (0, 0)
+  lcd.home ();
+}
+
+void loop() {
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed
+  //of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+  inches = pulse_width / 148.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    Serial.println("Out of range");
+  } else {
+    Serial.print(cm);
+    Serial.print(" cm \t");
+    Serial.print(inches);
+    Serial.println(" in");
+  }
+   // Wait at least 60ms before next measurement
+  delay(60);
+
+  // Imprimir "Hola Mundo" en la primera linea
+  lcd.setCursor ( 0, 0 );
+  lcd.print(cm);
+  lcd.print(" cm");
+  // Mover el cursor a la segunda linea (1) primera columna
+  lcd.setCursor ( 0, 1 );
+  // Imprimir otra cadena en esta posicion
+  lcd.print(inches);
+  lcd.print(" inches");
+  // Esperar un segundo
+  delay(60);
+}
